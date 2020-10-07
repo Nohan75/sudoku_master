299ef043e57dd96542a54ca05c0a1c26705f4f49
Nohan75 <nohanmarielouise@gmail.com>
2020-10-07 11:41:37 +0200

    Ajout de la fonction affichant la grille

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
+
+    public static void displayGrid(int[][] tab){
+        for(int i = 0; i < tab.length; i++){
+            for(int j = 0; j < tab[i].length; j++){
+                System.out.print(tab[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        int[][] boardEasy = {
+                {4, 0, 0, 1, 0, 2, 6, 8, 0},
+                {1, 0, 0, 0, 9, 0, 0, 0, 4},
+                {0, 3, 8, 0, 6, 4, 0, 1, 0},
+                {0, 0, 5, 0, 7, 1, 9, 2, 0},
+                {0, 2, 6, 0, 0, 9, 8, 0, 0},
+                {8, 0, 0, 2, 5, 0, 0, 0, 0},
+                {9, 0, 3, 0, 0, 0, 0, 0, 8},
+                {2, 5, 0, 6, 0, 0, 1, 0, 7},
+                {6, 0, 7, 9, 0, 5, 3, 0, 0}
+        };
+        displayGrid(boardEasy);
     }
 }
