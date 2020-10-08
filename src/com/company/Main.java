e464f8feee8640b0aa28af7d1d458ffb4950ead7
Nohan75 <nohanmarielouise@gmail.com>
2020-10-08 10:31:37 +0200

    Ajout de la fonction vérifiant la validité de la grille

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+
+import com.robin.Boards;
 import org.w3c.dom.ls.LSOutput;
 
 public class Main {
@@ -14,7 +16,7 @@ public class Main {
     }
 
     public static boolean checkLine(int tab[][],int line, int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
         for (int i = 0; i < tab[line].length; i++){
             if(tab[line][i] == number){
@@ -24,13 +26,13 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " sur la  ligne  = " + count);
+//        System.out.println("Nombre de " + number + " sur la  ligne  = " + count);
         return true;
     }
 
 
     public static boolean checkcolone(int tab[][],int colonne, int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
         for (int j = 0; j < tab[colonne].length; j++){
             if(tab[j][colonne] == number){
@@ -40,12 +42,12 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " sur la  colonne  = " + count);
+//        System.out.println("Nombre de " + number + " sur la  colonne  = " + count);
         return true;
     }
 
     public static boolean checkSection1(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
         for (int i = 0; i < 3; i++){
@@ -58,16 +60,16 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
     public static boolean checkSection2(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
         for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+            for (int j = 3; j < 6; j++){
                 if(tab[i][j] == number){
                     count++;
                 }
@@ -76,16 +78,16 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
     public static boolean checkSection3(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
         for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+            for (int j = 6; j < 9; j++){
                 if(tab[i][j] == number){
                     count++;
                 }
@@ -94,15 +96,15 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
     public static boolean checkSection4(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
-        for (int i = 0; i < 3; i++){
+        for (int i = 3; i < 6; i++){
             for (int j = 0; j < 3; j++){
                 if(tab[i][j] == number){
                     count++;
@@ -112,16 +114,16 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
     public static boolean checkSection5(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        for (int i = 3; i < 6; i++){
+            for (int j = 3; j < 6; j++){
                 if(tab[i][j] == number){
                     count++;
                 }
@@ -130,16 +132,16 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
     public static boolean checkSection6(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        for (int i = 3; i < 6; i++){
+            for (int j = 6; j < 9; j++){
                 if(tab[i][j] == number){
                     count++;
                 }
@@ -148,15 +150,15 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
     public static boolean checkSection7(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
-        for (int i = 0; i < 3; i++){
+        for (int i = 6; i < 9; i++){
             for (int j = 0; j < 3; j++){
                 if(tab[i][j] == number){
                     count++;
@@ -166,16 +168,16 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
     public static boolean checkSection8(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        for (int i = 6; i < 9; i++){
+            for (int j = 3; j < 6; j++){
                 if(tab[i][j] == number){
                     count++;
                 }
@@ -184,16 +186,16 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
     public static boolean checkSection9(int tab[][], int number){
-        System.out.println("------");
+//        System.out.println("------");
         int count = 0;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        for (int i = 6; i < 9; i++){
+            for (int j = 6; j < 9; j++){
                 if(tab[i][j] == number){
                     count++;
                 }
@@ -202,34 +204,30 @@ public class Main {
                 return false;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
+//        System.out.println("Nombre de " + number + " = " + count);
 
         return true;
     }
-    public static boolean checkSection(int tab[][], int number){
-        System.out.println("------");
-        int count = 0;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                if(tab[i][j] == number){
-                    count++;
-                }
-            }
-            if (count > 1){
-                return false;
+    public static boolean grilleValide(int[][] tab, int number){
+        System.out.println("---------");
+        for(int i = 0; i < 9; i++){
+            if(checkcolone(tab, i, number) && checkLine(tab, i, number) && checkSection1(tab, number)
+                    && checkSection2(tab, number) && checkSection3(tab, number) && checkSection4(tab, number)
+                    && checkSection5(tab, number) && checkSection6(tab, number) && checkSection7(tab, number)
+                    && checkSection8(tab, number) && checkSection9(tab, number)){
+
+                return true;
             }
         }
-        System.out.println("Nombre de " + number + " = " + count);
-
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
 	// write your code here
         int[][] boardEasy = {
                 {4, 0, 0, 1, 0, 2, 6, 8, 0},
-                {1, 0, 0, 0, 9, 0, 0, 0, 4},
+                {1, 0, 0, 9, 9, 0, 0, 0, 4},
                 {0, 3, 8, 0, 6, 4, 0, 1, 0},
                 {0, 0, 5, 0, 7, 1, 9, 2, 0},
                 {0, 2, 6, 0, 0, 9, 8, 0, 0},
@@ -238,10 +236,11 @@ public class Main {
                 {2, 5, 0, 6, 0, 0, 1, 0, 7},
                 {6, 0, 7, 9, 0, 5, 3, 0, 0}
         };
-        displayGrid(boardEasy);
 
-        System.out.println(checkLine(boardEasy, 0, 1));
-        System.out.println(checkcolone(boardEasy, 0,8));
-        System.out.println(checkSection(boardEasy, 2));
+        displayGrid(boardEasy);
+//        System.out.println(checkLine(boardEasy, 0, 1));
+//        System.out.println(checkcolone(boardEasy, 0,8));
+//        System.out.println(checkSection5(boardEasy, 9));
+        System.out.println(grilleValide(boardEasy, 9));
     }
 }
